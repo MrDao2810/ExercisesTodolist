2e15d56ed6604f2cdbf810f92d23527d978739cb
CodeBro <daoquyen833@gmail.com>
2022-05-19 21:54:26 +0700

    Update file Exercises todolist

---
+++ main.js
@@ -0,0 +1,9 @@
+let array = [];
+function listArray() {
+    
+}
+function myFunction() {
+    let x = document.createElement('INPUT');
+    x.setAttribute('type', 'checkbox');
+    document.body.appendChild(x);
+}
