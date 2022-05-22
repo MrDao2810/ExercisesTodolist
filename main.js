0615b06e4bb7a4ab7fb4ec890c3ef06dbd693521
create feature <dao.daoquyen833@gmail.com>
2022-05-23 01:26:56 +0700

    Update file tidolist

--- main.js
+++ main.js
@@ -10,31 +10,29 @@ function newElement() {
     
     // Tạo node p
     let p = document.createElement('P');
-
     // Lấy giá trị người dùng nhập
-    let listTaskString = document.getElementById('myInput').value;
-    
+    let listTaskString = document.getElementById('myInput').value;   
     // Tạo dối tượng text node 
     let t = document.createTextNode(listTaskString);
-
-    // Tạo thẻ input
-    let checkBoxElement = document.createElement('input');
-
-    // Cho input type = checkbox
-    checkBoxElement.setAttribute('type', 'checkbox');
-
-    // Thêm vào đầu của p
-    p.prepend(checkBoxElement); 
-
+    // Xoá giá trị thẻ input sau khi nhập
+    document.getElementById('myInput').value = '';
     // Gán text node cho thẻ p vừa tạo
     p.appendChild(t);
     document.getElementById('myUL').appendChild(p);
 
-    // tạo thẻ input
-    let tasksDelete = document.createElement('button');
+    // Tạo thẻ input
+    let checkBoxElement = document.createElement('INPUT');
+    // Cho input type = checkbox
+    checkBoxElement.setAttribute('type', 'checkbox');
+    // Thêm vào đầu của p
+    p.prepend(checkBoxElement); 
 
+    // tạo thẻ button 
+    let tasksDelete = document.createElement('BUTTON');
+    // thêm vào sau p
+    p.append(tasksDelete);
     tasksDelete.innerHTML = 'Delete'; // <button onclick="process">Hello</button>
-    // dùng addEvebtListener truyền vào 1 callback vd: click ='function(event)' 
+    // dùng addEvebtListener Thêm sự kiện cho đối tượng 
     tasksDelete.addEventListener('click', function(event) {
         const isDone = checkBoxElement.checked;
         isDone ? --doneCount : --undoneCount;
@@ -43,7 +41,9 @@ function newElement() {
         updateDoneView();
     });
 
+    // khi checkBoxElement (checkbox) được click thì sẽ gọi hàm 
     checkBoxElement.addEventListener('change', function(e) {
+        // target là thằng cuối cùng mà mình click vào
         if (e.target.checked) {
             ++doneCount;
             undoneCount = totalCount - doneCount;
@@ -55,16 +55,10 @@ function newElement() {
         }
         updateDoneView();
     });
-    // thêm vào sau p
-    p.append(tasksDelete);
-    // console.log(listTaskString);
-    // Xoá giá trị thẻ input sau khi nhập
-    document.getElementById('myInput').value = '';
     // In ra số lượng công việc đã add
     ++undoneCount;
     ++totalCount;
     updateDoneView();
-
 }
 
 function updateDoneView() {
@@ -72,3 +66,39 @@ function updateDoneView() {
     document.getElementById('done').innerHTML = 'Đã hoàn thành : ' + doneCount;
     document.getElementById('undone').innerHTML = 'Chưa hoàn thành : ' + undoneCount;
 }
+
+// document.addEventListener('dragstart', function(e) { // bắt đầu khi có sự kiện kéo - chỉ sảy ra 1 lần từ khi kích hoạt
+//     e.dataTransfer.setData('myData', e.target.id);
+//     // console.log('keo');
+// });
+// document.addEventListener('drag', function() { // bắt đầu khi thả chuột, kết thúc sự kiện kéo
+//     // console.log('dang chay');
+// });
+// document.addEventListener('dragend', function() { // bắt đầu khi có sự kiện kéo - và chạy liên tục từ khi kích hoạt
+//     console.log('nha chuot - ket thuc keo');
+// });
+// document.addEventListener('dragenter', function(e){
+//     if (e.target.id == 'myUL') {
+//         console.log('hung su kien');
+//     }
+// });
+// document.addEventListener('dragover', function(e){
+//     e.preventDefault();
+//     // if (e.target.id == 'myUL') {
+//     //     console.log('dang chay su kien hung drag');
+//     // }
+// });
+// document.addEventListener('dragleave', function(e){
+//     if (e.target.id == 'myUL') {
+
+//     }
+//     // console.log('nha chuot ket thuc hung keo');
+// });
+// document.addEventListener('drop', function(e){
+//     e.preventDefault();
+//     if (e.target.className == 'element') {
+//         let data = e.dataTransfer.getData('myUL');
+//         e.target.appendChild(document.getElementById(data));
+//     };
+//     console.log('nha thong tin');
+// });
